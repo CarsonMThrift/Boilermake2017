358dbadd08c96a4054d6dd652274041055374737
Nathalie Kroeker <nathaliekroeker@gmail.com>
2017-09-30 16:51:44 -0400

    Lovely lil switch/case for gabs

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-function initMap(destination) {
+function initMap(coords) {
     // TODO Get GPS location of device
 
-  destination = convertAddress(destination);
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: coords
+    });
 
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: destination
-  });
-
-  const destinationMarker = new google.maps.Marker({
-    position: destination,
-    map: map
-  });
+    const originMarker = new google.maps.Marker({
+      position: coords,
+      map: map
+    });
 }
 
-function convertAddress(address) {
+function convertAddress(address, action) {
   var geocoder = new google.maps.Geocoder();
   // var address = "8228 E 21st St, Indianapolis, Indiana 46219";
   let coords;
@@ -28,33 +26,32 @@ function convertAddress(address) {
       const long = parseFloat(longitude);
       coords = {lat: lat, lng: long};
 
-      const map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: coords
-      });
 
-      const originMarker = new google.maps.Marker({
-        position: coords,
-        map: map
-      });
+    }
+    switch(action) {
+        case 1:
+            initMap(coords);
+            break;
+        case 2:
+            getParking(coords);
+            break;
+        default:
+            break;
     }
   });
 
 
 }
 
-function getParking(destination) {
-  //   destination = convertAddress(destination);
-  //   console.log(destination);
-  //   const options = {
-  //     "url": `https://apis.solarialabs.com/shine/v1/parking-rules/meters?lat=${destination.lat}&long=${destination.lng}&apikey=3eXq0tri3mABFAZJapJX1uAvD0AZY1qP`,
-  //     "method": "GET",
-  //     "processData": false
-  //   }
-  //
-  //   $.ajax(options).done((response)=>{
-  //     console.log(response)
-  // });
+function getParking(coords) {
+    const options = {
+      "url": `https://apis.solarialabs.com/shine/v1/parking-rules/meters?lat=${coords.lat}&long=${coords.lng}&apikey=3eXq0tri3mABFAZJapJX1uAvD0AZY1qP`,
+      "method": "GET",
+      "processData": false
+    }
+    $.ajax(options).done((response)=>{
+        // setParkingData(response);
+  });
 }
 
 $(document).ready(function() {
@@ -62,11 +59,11 @@ $(document).ready(function() {
     const destination = $("#destination").val();
 
     // Call various API functions as we get them set up
-    document.querySelector('.output-container').style.display="block";
+    document.querySelector('.output-container').style.display="initial";
     document.querySelector('#map').style.display="block";
     document.querySelector('#footer').style.display="block";
-    initMap(destination);
-    getParking(destination);
+    convertAddress(destination, 1); // Initialize Map
+    convertAddress(destination, 2); // Get parking data
   });
 
 });
