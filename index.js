eb068b9a2f9d2453468ddb8f8e7052c0b59245e6
Gabriela Justice <gnjustic@iupui.edu>
2017-09-30 14:07:37 -0400

    define coords

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 function initMap(destination) {
+  const currentLocation = $("#currentLocation").val();
 
   const origin = convertAddress(currentLocation);
-<<<<<<< HEAD
-  const destination = convertAddress(destinationLocation);
-=======
-  // const destination = convertAddress(destination);
-  console.log(origin);
-
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: origin
-  });
-
-  const originMarker = new google.maps.Marker({
-    position: origin,
-    map: map
-  });
->>>>>>> 8b246732094bf77ec8635105da14c4a3bee3cde3
+  // destination = convertAddress(destination);
 
 
   // const destinationMarker = new google.maps.Marker({
@@ -31,25 +17,29 @@ function initMap(destination) {
 function convertAddress(address) {
   var geocoder = new google.maps.Geocoder();
   // var address = "8228 E 21st St, Indianapolis, Indiana 46219";
-
+  let coords;
+  
   geocoder.geocode( { 'address': address}, function(results, status) {
 
     if (status == google.maps.GeocoderStatus.OK) {
       var latitude = results[0].geometry.location.lat();
       var longitude = results[0].geometry.location.lng();
       coords = {lat: `${latitude}`, lng: `${longitude}`};
+      console.log(coords)
+
+      const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: coords
+      });
+    
+      const originMarker = new google.maps.Marker({
+        position: coords,
+        map: map
+      });
     }
   });
 
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: coords
-  });
-
-  const originMarker = new google.maps.Marker({
-    position: coords,
-    map: map
-  });
+  
 }
 
 function getParking() {
