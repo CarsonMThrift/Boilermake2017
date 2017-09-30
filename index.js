cd5360d1b6c26a7d297cca10366ecedc211e72ce
David Purdum <purdum41@gmail.com>
2017-09-30 14:59:59 -0400

    Adaptability

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ $(document).ready(function() {
     const destination = $("#destination").val();
 
     // Call various API functions as we get them set up
-    document.querySelector('.output-container').style.display="initial";
+    document.querySelector('.output-container').style.display="block";
     document.querySelector('#map').style.display="block";
     document.querySelector('#footer').style.display="block";
     initMap(destination);
