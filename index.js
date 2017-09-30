09d0208b2fb99cd6bf17d38756ee55b0c3b00dbf
Gabriela Justice <gnjustic@iupui.edu>
2017-09-30 11:08:45 -0400

    tries to convert address into coordinates

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-function initMap() {
-  const currentLocation = $("#currentLocation").val().split(",");
-  const destination = $("#destination").val().split(",");
 
-  const uluru = { lat: parseFloat(currentLocation[0]), lng: parseFloat(currentLocation[1]) }
+
+function initMap() {
+  const currentLocation = $("#currentLocation").val();
+  const destinationLocation = $("#destination").val();
+
+  const origin = convertAddress(currentLocation);
+  // const destination = convertAddress(destinationLocation);
+  console.log(origin);
 
   const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: uluru
+    center: origin
   });
 
-  const marker = new google.maps.Marker({
-    position: uluru,
+  const originMarker = new google.maps.Marker({
+    position: origin,
     map: map
   });
+  
 
+  // const destinationMarker = new google.maps.Marker({
+  //   position: destination,
+  //   map: map
+  // });
+
+
+}
+
+function convertAddress(address) {
+  var geocoder = new google.maps.Geocoder();
+  // var address = "8228 E 21st St, Indianapolis, Indiana 46219";
+  
+  geocoder.geocode( { 'address': address}, function(results, status) {
+  
+  if (status == google.maps.GeocoderStatus.OK) {
+    var latitude = results[0].geometry.location.lat();
+    var longitude = results[0].geometry.location.lng();
+    const coords = {lat: `${latitude}`, lng: `${longitude}`};
+    console.log(coords);
+    return coords;
+  } 
+  });
 }
 
 // function initMap() {
